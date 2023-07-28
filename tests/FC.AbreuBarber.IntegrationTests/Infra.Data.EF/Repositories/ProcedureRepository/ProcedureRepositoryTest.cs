eb84625174d27aa351e009725e9e3ccd2bc53f38
Alexandre Abreu <abreuxandi2010@gmail.com>
2023-07-28 11:09:55 -0300

    feat: create procedure repository inser test

--- tests/FC.AbreuBarber.IntegrationTests/Infra.Data.EF/Repositories/ProcedureRepository/ProcedureRepositoryTest.cs
+++ tests/FC.AbreuBarber.IntegrationTests/Infra.Data.EF/Repositories/ProcedureRepository/ProcedureRepositoryTest.cs
@@ -9,5 +9,32 @@ namespace FC.AbreuBarber.IntegrationTests.Infra.Data.EF.Repositories.ProcedureRe
     public class ProcedureRepositoryTest
     {
 
+        private readonly ProcedureRepositoryTestFixture _fixture;
+
+        public ProcedureRepositoryTest(ProcedureRepositoryTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact(DisplayName = nameof(Insert))]
+        [Trait("Integration/Infra.Data", "ProcedureRepository - Repositories")]
+        public async Task Insert()
+        {
+            AbreuBarberDbContext dbContext = _fixture.CreateDbContext();
+            var exampleProcedure = _fixture.GetExampleProcedure();
+            var procedureRepository = new ProcedureRepository(dbContext);
+
+            await procedureRepository.Insert(exampleProcedure, CancellationToken.None);
+            await dbContext.SaveChangesAsync();
+
+            var dbProcedure = await dbContext.Procedures.Find(exampleProcedure.Id);
+
+            dbProcedure.Should().NotBeNull();
+            dbProcedure.Name.Should().Be(exampleProcedure.Name);
+            dbProcedure.Description.Should().Be(exampleProcedure.Description);
+            dbProcedure.Value.Should().Be(exampleProcedure.Value);
+            dbProcedure.IsAcive.Should().Be(exampleProcedure.IsActive);
+            dbProcedure.CreatedAt.Should().Be(exampleProcedure.CreatedAt);
+        }
     }
 }
--- tests/FC.AbreuBarber.IntegrationTests/Infra.Data.EF/Repositories/ProcedureRepository/ProcedureRepositoryTestFixture.cs
+++ tests/FC.AbreuBarber.IntegrationTests/Infra.Data.EF/Repositories/ProcedureRepository/ProcedureRepositoryTestFixture.cs
@@ -1,5 +1,7 @@
 ﻿
+using FC.AbreuBarber.Domain.Entity;
 using FC.AbreuBarber.IntegrationTests.Base;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace FC.AbreuBarber.IntegrationTests.Infra.Data.EF.Repositories.ProcedureRepository
@@ -9,5 +11,61 @@ namespace FC.AbreuBarber.IntegrationTests.Infra.Data.EF.Repositories.ProcedureRe
 
     public class ProcedureRepositoryTestFixture : BaseFixture
     {
+        public string GetValidProcedureName()
+        {
+            var procedureName = "";
+            while (procedureName.Length < 3)
+                procedureName = GenerateProcedureName();
+            if (procedureName.Length > 255)
+                procedureName = procedureName[..255];
+            return procedureName;
+        }
+
+        public string GetValidProcedureDescription()
+        {
+            var procedureDescription = GenerateProcedureDescription();
+            if (procedureDescription.Length > 10_000)
+                procedureDescription = procedureDescription[..10_000];
+            return procedureDescription;
+        }
+
+        public double GetValidProcedureValue()
+        {
+            var procedureValue = Faker.Random.Double(30, 1000);
+
+            return procedureValue;
+        }
+
+        public bool getRandomBoolean()
+        => new Random().NextDouble() < 0.5;
+
+        private string GenerateProcedureName()
+        {
+            var procedureName = Faker.Lorem.Sentence(2);
+            return procedureName;
+        }
+
+        private string GenerateProcedureDescription()
+        {
+            var procedureDescription = Faker.Lorem.Paragraph();
+            return procedureDescription;
+        }
+
+        public Procedure GetExampleProcedure()
+        {
+            return new(GetValidProcedureName(), GetValidProcedureDescription(), GetValidProcedureValue(), getRandomBoolean());
+        }
+
+
+        public AbreuBarberDbContext CreateDbContext()
+        {
+            var dbContext = new AbreuBarberDbContext(
+                new DbContextOptionsBuilder<AbreuBarberDbContext>()
+                .UseInMemoryDatabase("integration-tests-db")
+                .Options
+                );
+
+            return dbContext;
+        }
     }
 }
